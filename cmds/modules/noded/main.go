threefoldtech/zos

init node location updater: - add updater package - expose GetState from registerer - fix longitude spelling

cmds/modules/noded/main.go
@@ -23,6 +23,7 @@ import (
 	"github.com/threefoldtech/zos/pkg/perf/publicip"
 	"github.com/threefoldtech/zos/pkg/registrar"
 	"github.com/threefoldtech/zos/pkg/stubs"
+	"github.com/threefoldtech/zos/pkg/updater"
 	"github.com/threefoldtech/zos/pkg/utils"
 
 	"github.com/rs/zerolog/log"
@@ -161,6 +162,15 @@ func action(cli *cli.Context) error {
 		WithVirtualized(len(hypervisor) != 0)
 
 	go registerationServer(ctx, msgBrokerCon, env, info)
+
+	log.Info().Msg("start node updater")
+	nodeUpdater := updater.NewUpdater(redis)
+	if err != nil {
+		return errors.Wrap(err, "failed to create new node updater")
+	}
+
+	go nodeUpdater.Start(ctx)
+
 	log.Info().Msg("start perf scheduler")
 
 	perfMon, err := perf.NewPerformanceMonitor(msgBrokerCon)

pkg/registrar/registrar.go
@@ -10,18 +10,18 @@ import (
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 	"github.com/threefoldtech/zbus"
+	"github.com/threefoldtech/zos/pkg"
 	"github.com/threefoldtech/zos/pkg/app"
 	"github.com/threefoldtech/zos/pkg/environment"
 	"github.com/threefoldtech/zos/pkg/stubs"
 )
 
 // should any of this be moved to pkg?
-type RegistrationState string
 
 const (
-	Failed     RegistrationState = "Failed"
-	InProgress RegistrationState = "InProgress"
-	Done       RegistrationState = "Done"
+	Failed     pkg.RegistrationState = "Failed"
+	InProgress pkg.RegistrationState = "InProgress"
+	Done       pkg.RegistrationState = "Done"
 
 	monitorAccountEvery = 30 * time.Minute
 )
@@ -31,15 +31,8 @@ var (
 	ErrFailed     = errors.New("registration failed")
 )
 
-type State struct {
-	NodeID uint32
-	TwinID uint32
-	State  RegistrationState
-	Msg    string
-}
-
-func FailedState(err error) State {
-	return State{
+func FailedState(err error) pkg.State {
+	return pkg.State{
 		0,
 		0,
 		Failed,
@@ -47,8 +40,8 @@ func FailedState(err error) State {
 	}
 }
 
-func InProgressState() State {
-	return State{
+func InProgressState() pkg.State {
+	return pkg.State{
 		0,
 		0,
 		InProgress,
@@ -56,8 +49,8 @@ func InProgressState() State {
 	}
 }
 
-func DoneState(nodeID uint32, twinID uint32) State {
-	return State{
+func DoneState(nodeID uint32, twinID uint32) pkg.State {
+	return pkg.State{
 		nodeID,
 		twinID,
 		Done,
@@ -66,13 +59,13 @@ func DoneState(nodeID uint32, twinID uint32) State {
 }
 
 type Registrar struct {
-	state State
+	state pkg.State
 	mutex sync.RWMutex
 }
 
 func NewRegistrar(ctx context.Context, cl zbus.Client, env environment.Environment, info RegistrationInfo) *Registrar {
 	r := Registrar{
-		State{
+		pkg.State{
 			0,
 			0,
 			InProgress,
@@ -84,13 +77,13 @@ func NewRegistrar(ctx context.Context, cl zbus.Client, env environment.Environme
 	return &r
 }
 
-func (r *Registrar) setState(s State) {
+func (r *Registrar) setState(s pkg.State) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
 	r.state = s
 }
 
-func (r *Registrar) getState() State {
+func (r *Registrar) GetState() pkg.State {
 	r.mutex.RLock()
 	defer r.mutex.RUnlock()
 	return r.state
@@ -162,11 +155,11 @@ func (r *Registrar) reActivate(ctx context.Context, cl zbus.Client, env environm
 }
 
 func (r *Registrar) NodeID() (uint32, error) {
-	return r.returnIfDone(r.getState().NodeID)
+	return r.returnIfDone(r.GetState().NodeID)
 }
 
 func (r *Registrar) TwinID() (uint32, error) {
-	return r.returnIfDone(r.getState().TwinID)
+	return r.returnIfDone(r.GetState().TwinID)
 }
 
 func (r *Registrar) returnIfDone(v uint32) (uint32, error) {

threefoldtech/zoslight

cmds/modules/noded/main.go
@@ -23,6 +23,7 @@ import (
 	"github.com/threefoldtech/zos4/pkg/perf/publicip"
 	"github.com/threefoldtech/zos4/pkg/registrar"
 	"github.com/threefoldtech/zos4/pkg/stubs"
+	"github.com/threefoldtech/zos4/pkg/updater"
 	"github.com/threefoldtech/zos4/pkg/utils"
 
 	"github.com/rs/zerolog/log"
@@ -156,6 +157,15 @@ func action(cli *cli.Context) error {
 		WithVirtualized(len(hypervisor) != 0)
 
 	go registerationServer(ctx, msgBrokerCon, env, info)
+
+	log.Info().Msg("start node updater")
+	nodeUpdater := updater.NewUpdater(redis)
+	if err != nil {
+		return errors.Wrap(err, "failed to create new node updater")
+	}
+
+	go nodeUpdater.Start(ctx)
+
 	log.Info().Msg("start perf scheduler")
 
 	perfMon, err := perf.NewPerformanceMonitor(msgBrokerCon)

pkg/registrar/registrar.go
@@ -10,18 +10,18 @@ import (
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 	"github.com/threefoldtech/zbus"
+	"github.com/threefoldtech/zos4/pkg"
 	"github.com/threefoldtech/zos4/pkg/app"
 	"github.com/threefoldtech/zos4/pkg/environment"
 	"github.com/threefoldtech/zos4/pkg/stubs"
 )
 
 // should any of this be moved to pkg?
-type RegistrationState string
 
 const (
-	Failed     RegistrationState = "Failed"
-	InProgress RegistrationState = "InProgress"
-	Done       RegistrationState = "Done"
+	Failed     pkg.RegistrationState = "Failed"
+	InProgress pkg.RegistrationState = "InProgress"
+	Done       pkg.RegistrationState = "Done"
 
 	monitorAccountEvery = 30 * time.Minute
 )
@@ -31,15 +31,8 @@ var (
 	ErrFailed     = errors.New("registration failed")
 )
 
-type State struct {
-	NodeID uint32
-	TwinID uint32
-	State  RegistrationState
-	Msg    string
-}
-
-func FailedState(err error) State {
-	return State{
+func FailedState(err error) pkg.State {
+	return pkg.State{
 		0,
 		0,
 		Failed,
@@ -47,8 +40,8 @@ func FailedState(err error) State {
 	}
 }
 
-func InProgressState() State {
-	return State{
+func InProgressState() pkg.State {
+	return pkg.State{
 		0,
 		0,
 		InProgress,
@@ -56,8 +49,8 @@ func InProgressState() State {
 	}
 }
 
-func DoneState(nodeID uint32, twinID uint32) State {
-	return State{
+func DoneState(nodeID uint32, twinID uint32) pkg.State {
+	return pkg.State{
 		nodeID,
 		twinID,
 		Done,
@@ -66,13 +59,13 @@ func DoneState(nodeID uint32, twinID uint32) State {
 }
 
 type Registrar struct {
-	state State
+	state pkg.State
 	mutex sync.RWMutex
 }
 
 func NewRegistrar(ctx context.Context, cl zbus.Client, env environment.Environment, info RegistrationInfo) *Registrar {
 	r := Registrar{
-		State{
+		pkg.State{
 			0,
 			0,
 			InProgress,
@@ -84,13 +77,13 @@ func NewRegistrar(ctx context.Context, cl zbus.Client, env environment.Environme
 	return &r
 }
 
-func (r *Registrar) setState(s State) {
+func (r *Registrar) setState(s pkg.State) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
 	r.state = s
 }
 
-func (r *Registrar) getState() State {
+func (r *Registrar) GetState() pkg.State {
 	r.mutex.RLock()
 	defer r.mutex.RUnlock()
 	return r.state
@@ -162,11 +155,11 @@ func (r *Registrar) reActivate(ctx context.Context, cl zbus.Client, env environm
 }
 
 func (r *Registrar) NodeID() (uint32, error) {
-	return r.returnIfDone(r.getState().NodeID)
+	return r.returnIfDone(r.GetState().NodeID)
 }
 
 func (r *Registrar) TwinID() (uint32, error) {
-	return r.returnIfDone(r.getState().TwinID)
+	return r.returnIfDone(r.GetState().TwinID)
 }
 
 func (r *Registrar) returnIfDone(v uint32) (uint32, error) {